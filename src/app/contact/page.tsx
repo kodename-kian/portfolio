969fc4e6c99e80cc3396dd085803169f99e9fa99
kodename-kian <kodename.kian@gmail.com>
2024-08-08 11:58:27 +0800

    added primeicons, fixed socials

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,15 +4,14 @@ import Chunk from "@/components/Chunk";
 import ContactForm from "@/components/ContactForm";
 
 export default function Home() {
-    // TODO: logos for socials
     return (
       <Body>
         <div className="w-full md:h-full flex flex-col items-center md:flex-row md:justify-center p-2 md:p-4">
           <div className="w-full md:h-full p-2 md:w-1/2"> 
             <span className="text-lg"> Find Me Here: </span>
-            <Chunk href="https://github.com/kodename-kian" label="GitHub" />
-            <Chunk href="https://www.linkedin.com/in/kian-colin-chua/" label="LinkedIn" />
-            <Chunk href="https://www.instagram.com/kian.colin/" label="Instagram" />
+            <Chunk href="https://github.com/kodename-kian" label="GitHub" icon="pi pi-github" />
+            <Chunk href="https://www.linkedin.com/in/kian-colin-chua/" label="LinkedIn" icon="pi pi-linkedin" />
+            <Chunk href="https://www.instagram.com/kian.colin/" label="Instagram" icon="pi pi-instagram" />
           </div>
           <div className="w-full md:h-full md:w-1/2 m-2 border-dotted border border-black p-2 overflow-y-auto">
             <div className="w-full">
--- src/components/Chunk.tsx
+++ src/components/Chunk.tsx
@@ -3,15 +3,17 @@ import React from 'react';
 interface ChunkProps {
     href?: string,
     label: string,
+    icon?: string,
 }
 
-export default function Chunk({href, label} : ChunkProps) {
+export default function Chunk({href, label, icon} : ChunkProps) {
     return (
         <div className="w-full mt-2 mb-2 border-2 border-gray-400 active:border-black rounded-sm">
             <a href={href} target="_blank" rel="noopener noreferrer" 
                className="w-full flex border-2 border-white border-b-gray-500 border-r-gray-500 active:border-transparent
-                          bg-gray-300 hover:bg-gray-400 p-2 text-black">
-                {label}
+                          bg-gray-300 hover:bg-gray-400 p-2 text-black items-center">
+                { icon ? <span className={icon + " pr-2 text-lg"}> </span> : <></>}
+                { label }
             </a>
         </div>
     );
