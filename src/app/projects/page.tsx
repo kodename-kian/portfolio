369ebb23e3e2f0ef57a6700e5f8d4d4d974d8fbd
kodename-kian <kodename.kian@gmail.com>
2024-08-08 16:55:02 +0800

    project skeleton + content component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Body from "@/components/Body";
-import Chunk from "@/components/Chunk";
+import Project from "@/components/Project";
 
 export default function Home() {
     return (
       <Body>
-        <div>
-          My Projects
+        <div className="overflow-y-auto">
+          <span className="text-2xl"> &gt;&gt; 📁 Archive </span>
+          <div className="h-full flex flex-col items-left ml-4 mr-4 p-4">
+            <span className="w-full text-xl      mb-2 border-b border-black"> Code </span>
+            <span className="w-full text-xl mt-4 mb-2 border-b border-black"> Leadership </span>
+              <Project title="TEDxYouth@SJCS" year="2022" open={true} links={[{platform: "HIGHLIGHTS", url:"https://www.youtube.com/watch?v=cwNiavtLFlw", icon:"pi pi-youtube"},
+                                                                              {platform: "ANOTHER ONE", url:"", icon:"pi pi-youtube"}
+              ]}>
+                [Content]
+              </Project>
+            <span className="w-full text-xl mt-4 mb-2 border-b border-black"> Writing  </span>
+              <Project title="IB MATH IA" />
+          </div>
         </div>
       </Body>
     );
---
+++ src/components/Project.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import React from 'react';
+import { useState, PropsWithChildren } from 'react';
+
+interface ProjectLinks {
+    platform: string,
+    url: string,
+    icon: string,
+}
+
+interface ProjectProps {
+    title: string,
+    year?: string,
+    open?: boolean,
+    links?: Array<ProjectLinks>,
+}
+
+export default function Project({title, year, open, links, children} : PropsWithChildren<ProjectProps>) {
+    const [contentVisible, setContentVis] = useState(open);
+
+    return (
+        <div>
+            <button onClick={() => setContentVis(!contentVisible)}>
+                { contentVisible ? <span className="text-md"> {'\u25BC'} </span> :
+                                   <span className="text-sm"> {'\u25B6'} </span> }
+                { title }
+                { year ? " (" + year + ")" : ""}
+            </button>
+
+            { contentVisible ?
+                <div className="flex flex-col p-2 rounded-lg border border-dashed border-black">
+                    { children }
+                    { links ? 
+                        <div className="flex flex-col md:flex-row md:items-center pt-6"> 
+
+                            { links.map((link) => {
+                                return( 
+                                    <a href={link.url} key={link.url} target="_blank" rel="noopener noreferrer"
+                                       className="md:w-40 border-2 border-white border-b-gray-500 border-r-gray-500 active:border-transparent
+                                                  bg-gray-300 hover:bg-gray-400 p-1 mr-1 text-black text-sm items-center"> 
+                                        <i className={link.icon + " pr-1 text-md"}></i> {link.platform} 
+                                    </a>
+                                );
+                            })} 
+                        
+                        </div> : <></>
+                    }
+                </div> : <></>
+            }
+        </div>
+    )
+}
